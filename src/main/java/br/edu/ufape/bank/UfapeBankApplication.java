57c6ce7ee90fc34546bda0cc6c8757a15021bbfa
JuanGustah <juangt360graus@gmail.com>
2024-01-16 13:57:40 -0300

    initial commit

---
+++ src/main/java/br/edu/ufape/bank/UfapeBankApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfapeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfapeBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ufape/bank/UfapeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfapeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
