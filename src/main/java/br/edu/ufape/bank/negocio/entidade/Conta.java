d23920422967c95dfe60edbe4a3c42ac9876fa6a
JuanGustah <juangt360graus@gmail.com>
2024-01-16 16:32:18 -0300

    feat: add contaBonificada entity

--- src/main/java/br/edu/ufape/bank/negocio/entidade/Conta.java
+++ src/main/java/br/edu/ufape/bank/negocio/entidade/Conta.java
@@ -2,8 +2,11 @@ package br.edu.ufape.bank.negocio.entidade;
 
 import br.edu.ufape.bank.negocio.excecao.conta.SaldoInsuficienteException;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Conta extends ContaAbstrata{
     public Conta() {
     }
---
+++ src/main/java/br/edu/ufape/bank/negocio/entidade/ContaBonificada.java
@@ -0,0 +1,31 @@
+package br.edu.ufape.bank.negocio.entidade;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Transient;
+
+@Entity
+public class ContaBonificada extends Conta{
+    private double bonus;
+    @Transient
+    private double taxaBonus;
+
+    public ContaBonificada() {
+        this.taxaBonus = 0.1;
+    }
+
+    @Override
+    public void creditar(double valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException();
+        }
+        bonus += (valor * taxaBonus);
+        super.creditar(valor);
+    }
+
+    public void renderBonus() {
+        super.creditar(bonus);
+        bonus = 0;
+    }
+}
